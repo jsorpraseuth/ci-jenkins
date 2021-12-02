5e12a3e6da45f693861ee61ea12262079f7956e2
james <sorpr002@cougars.csusm.edu>
2021-12-02 15:14:56 -0800

    refactor and develop sample test case

---
+++ src/main/java/test/testGame.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+import org.psnbtech.Game;
+
+import java.lang.reflect.Method;
+
+public class testGame {
+
+    @Test
+    public void testStartGame() throws NoSuchMethodException {
+        System.out.println("Test new game creation");
+    }
+
+}
