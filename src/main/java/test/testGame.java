b194f76d67b6ca419581a66dd840354bc26f5c8b
james <sorpr002@cougars.csusm.edu>
2021-12-02 15:46:30 -0800

    Update testGame.java

--- src/main/java/test/testGame.java
+++ src/main/java/test/testGame.java
@@ -1,11 +1,15 @@
 package test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.psnbtech.Game;
 
 import java.lang.reflect.Method;
 
+import static org.junit.Assert.*;
+
 public class testGame {
+
 
     @Test
     public void testStartGame() throws Exception {
@@ -15,6 +19,13 @@ public class testGame {
     @Test
     public void testResetGame() throws Exception {
         System.out.println("Test reset game");
+        assertEquals(0, )
+    }
+
+    @Test
+    public void testFail() throws Exception {
+        // intentional fail
+        fail("Not yet implemented");
     }
 
 }
