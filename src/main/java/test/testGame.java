66b971845bc86f430e0fa38aeab7eeee85a90bf6
james <sorpr002@cougars.csusm.edu>
2021-12-02 15:32:38 -0800

    Update testGame.java

--- src/main/java/test/testGame.java
+++ src/main/java/test/testGame.java
@@ -8,8 +8,13 @@ import java.lang.reflect.Method;
 public class testGame {
 
     @Test
-    public void testStartGame() throws NoSuchMethodException {
+    public void testStartGame() throws Exception {
         System.out.println("Test new game creation");
+    }
+
+    @Test
+    public void testResetGame() throws Exception {
+        System.out.println("Test reset game");
     }
 
 }
