d73d8c2ead5b261dc243eac3a43ec6a4c9809e12
james <sorpr002@cougars.csusm.edu>
2021-12-06 13:48:16 -0800

    add junit dependency, add clocktest

---
+++ src/main/java/org/psnbtech/util/ClockTest.java
@@ -0,0 +1,23 @@
+package org.psnbtech.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ClockTest {
+
+    @Test
+    public void simpleTest() {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 3);
+    }
+
+    @Test
+    public void simpleTestFail() {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 2);
+    }
+
+}
